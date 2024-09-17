a05cfa5b07b7d33f133e5ca2ab02bdeae18e8226
krishnadev7 <krishnadevv12@gmail.com>
2024-09-17 12:56:41 +0530

    installed redux tookit

---
+++ client/src/app/DashboardWrapper.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
+import StoreProvider from "./redux";
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+            <Sidebar/>
+            <main className="dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64">
+                <Navbar/>
+                {children}
+            </main>
+        </div>
+    )
+}
+
+const DashboardWrapper = ({children}: {children: ReactNode}) => {
+    return (
+        <StoreProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+        </StoreProvider>
+    )
+}
+
+export default DashboardWrapper;
