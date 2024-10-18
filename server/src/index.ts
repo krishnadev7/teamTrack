5ff0a739b500a91b76a7f78f3498c868cbce463a
krishnadev7 <krishnadevv12@gmail.com>
2024-10-18 14:38:07 +0530

    added prisma migrate and seed db in workflow

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req,res) => {
-  res.send(`TeamTrack AWS EC2 server is up and running on port:${port}`);
+  res.send(`TeamTrack AWS EC2 server is up and running on port:${port} and checking seed is working`);
 })
 
 // Routes
