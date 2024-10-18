1b79cb332e611c3b850b91f0b4a28ffad04d8d2e
krishnadev7 <krishnadevv12@gmail.com>
2024-10-18 11:03:38 +0530

    dockerfile updated and running

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req,res) => {
-  res.send(`TeamTrack server is up and running on port:${port}`);
+  res.send(`TeamTrack EC2 server is up and running on port:${port}`);
 })
 
 // Routes
